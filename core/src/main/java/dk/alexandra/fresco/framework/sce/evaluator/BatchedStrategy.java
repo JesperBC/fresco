a954654c2641655bfdaf3fdc984c781d6cec4987
Thomas Luxhøj <thomas.luxhoj@partisia.com>
2024-04-15 15:45:41 +0200

    Added logging

--- core/src/main/java/dk/alexandra/fresco/framework/sce/evaluator/BatchedStrategy.java
+++ core/src/main/java/dk/alexandra/fresco/framework/sce/evaluator/BatchedStrategy.java
@@ -6,6 +6,8 @@ import dk.alexandra.fresco.framework.ProtocolCollection;
 import dk.alexandra.fresco.framework.network.Network;
 import dk.alexandra.fresco.framework.sce.resources.ResourcePool;
 import java.util.Iterator;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  * This class implements the core of a general batched communication strategy for evaluating
@@ -31,6 +33,8 @@ import java.util.Iterator;
 public class BatchedStrategy<ResourcePoolT extends ResourcePool>
     implements BatchEvaluationStrategy<ResourcePoolT> {
 
+  private static final Logger logger = LoggerFactory.getLogger(BatchedStrategy.class);
+
   @Override
   public void processBatch(
       ProtocolCollection<ResourcePoolT> protocols, ResourcePoolT resourcePool,
@@ -49,6 +53,7 @@ public class BatchedStrategy<ResourcePoolT extends ResourcePool>
     Iterator<NativeProtocol<?, ResourcePoolT>> iterator = protocols.iterator();
     while (iterator.hasNext()) {
       NativeProtocol<?, ResourcePoolT> protocol = iterator.next();
+      logger.trace(protocol.getClass().getName());
       EvaluationStatus status = protocol.evaluate(round, rp, sceNetwork);
       if (status.equals(EvaluationStatus.IS_DONE)) {
         iterator.remove();
